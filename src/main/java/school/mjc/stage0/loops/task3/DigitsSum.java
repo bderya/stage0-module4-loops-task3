18fac56a2b479dc3edb63e6d453fc7cfcfb326c7
Batuhan Derya <bderya@3-G-9.42heilbronn.de>
2022-09-25 23:12:50 +0200

    Test1

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-
+		t = Math.abs(t);
+        int len = ("" + t).length();
+        int sum = 0;
+        for (int i = 0; i < len; i++) {
+            sum += t % 10;
+            t = t / 10;
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+		int sum = 0;
+        String number = "";
+        for (int i = 0; i < lengthOfLastNumber; i++) 
+		{
+            number += "9";
+            int value = Integer.parseInt(number);
+            sum += value;
+        }
+        System.out.println(sum);
     }
 }
